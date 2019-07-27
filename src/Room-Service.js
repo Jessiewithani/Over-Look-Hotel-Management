2611c87707c4e3e0adf71f3ed9a2a88c9f6488cb
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-07-26 20:40:09 -0600

    Add fetch request and add and remove user class and customer class

---
+++ src/Room-Service.js
@@ -0,0 +1,10 @@
+class RoomService{
+    constructor() {
+        this.id = id;
+        this.date = date;
+        this.food = food;
+        this.totalCost = totalCost;
+    }
+}
+
+export default RoomService;
