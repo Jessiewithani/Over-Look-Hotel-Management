33266d6d4c885d84641a7e1dbe7a7875b8ec759d
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-07-29 11:00:19 -0600

    Make customer class

--- src/Customers.js
+++ src/Customers.js
@@ -1,7 +1,41 @@
 class Customers {
-    constructor() {
-        // this.id = id;
-        // this.name = name;
+    constructor(sampleData) {
+        this.sampleData = sampleData;
+    }
+    displayUserInfo(name) {
+        let hotelGuest = this.sampleData.users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()))
+        return hotelGuest;
+    }
+    showAllRoomServiceTransactionsForUser(id) {
+        return this.sampleData.roomServices.filter(service => {
+          if (service.userID === id) {
+            return service;
+          }
+        })
+    }
+    roomServiceTransactionFeeByDatePerUser(date, id) {
+        return this.sampleData.roomServices.reduce((acc, curr) => {
+          if (curr.date === date && curr.userID == id) {
+            acc += curr.totalCost;
+          }
+          return acc;
+        }, 0);
+    }
+    calculateTotalRoomServiceFeesForEntireDuration(id) {
+        let findEntireTotal = this.sampleData.roomServices.filter(roomService => {
+          return roomService.userID === id
+        }).reduce((acc,curr) => {
+          acc += curr.totalCost
+          return acc;
+        },0)
+        return Number(findEntireTotal.toFixed(2))
+    }
+    calculateOrderFeesForSpecifiedDate(id, date) {
+        // console.log(this.sampleData)
+        let findUserId = this.sampleData.roomServices.filter(roomService => roomService.userID === id).find(user => {
+          return user.date === date
+        }).totalCost
+        return findUserId;
     }
 }
 
